688b76dbc532efa2e50ef0ce93ee456f3e814aaf
Rob Derstadt <rderstadt@gmail.com>
2018-11-13 13:26:48 -0700

    Bug fixes

--- src/evilbrisket_board.c
+++ src/evilbrisket_board.c
@@ -19,13 +19,14 @@ All rights reserved.
 bool init() {
  
     if (!bcm2835_init() || !bcm2835_spi_begin()) {
+	   printf("*** ERROR: Unable to initialize bcm2835 ***\n");
        return 0;
     }
 	   
 	// LTC2983 SPI
 	bcm2835_spi_chipSelect(BCM2835_SPI_CS0);  
     bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, LOW);  
-	bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_128);
+	bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_512);
     bcm2835_spi_setDataMode(BCM2835_SPI_MODE0);                  
  	bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);     
 
@@ -41,9 +42,11 @@ bool init() {
 	bcm2835_gpio_fsel(RPI_GPIO_P1_16, BCM2835_GPIO_FSEL_OUTP);
 	bcm2835_gpio_write(RPI_GPIO_P1_16, LOW);
 
-	// LTC2983 interupt
+	// LTC2983 status
 	bcm2835_gpio_fsel(RPI_V2_GPIO_P1_38, BCM2835_GPIO_FSEL_INPT);
-	bcm2835_gpio_hen(RPI_V2_GPIO_P1_38);
+
+	// LTC2983 Reset
+	bcm2835_gpio_fsel(RPI_BPLUS_GPIO_J8_40, BCM2835_GPIO_FSEL_OUTP);
 	
 	return init_for_probe(THERMOWORKS);
 }
@@ -53,33 +56,23 @@ bool init_for_probe(eb_manufacturer manufacturer) {
 	// GPIO 21 (physical pin 40) is reset. 
 	// While this pin is LOW, the device is forced into the reset state. 
 	//Once this pin is returned HIGH, the device initiates its start-up sequence. 	
-	bcm2835_gpio_fsel(RPI_BPLUS_GPIO_J8_40, BCM2835_GPIO_FSEL_OUTP);
-	bcm2835_gpio_write(RPI_BPLUS_GPIO_J8_40, LOW);
 	
-	delay(50);
+	bcm2835_gpio_write(RPI_BPLUS_GPIO_J8_40, LOW);
 	
 	// Enter initialization state on LTC2983
 	bcm2835_gpio_write(RPI_BPLUS_GPIO_J8_40, HIGH);
 	
-	delay(100);
-	
-	// Make sure it's ready to be configured
-	if (bcm2835_gpio_eds(RPI_V2_GPIO_P1_38)) {
-	        
-	 	// Clear the eds flag 
-		bcm2835_gpio_set_eds(RPI_V2_GPIO_P1_38);
+	// Wait for LTC2983 ready state
+	while (0 == bcm2835_gpio_lev(RPI_V2_GPIO_P1_38));
 	      
-		// Initialize the LTC2983 with Evil Brisket board config
-		configure_channels();			
-		configure_memory_table(manufacturer);
-		configure_global_parameters();
+	// Initialize the LTC2983 with Evil Brisket board config
+	configure_channels();			
+	configure_memory_table(manufacturer);
+	configure_global_parameters();
 		
-		//printf("*** LTC2983 is ready ***\n");
+	printf("*** LTC2983 is ready ***\n");
 			
-	} else {
-		printf("*** Unable to initialize LTC2983 ***");
-		return 0;
-	}
+	return 0;
 }
 
 void led(eb_led led, bool enable) {
